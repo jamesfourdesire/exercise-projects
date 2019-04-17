b53607601f95eb46f93a15fb4f6922a26e0d1ced
James Chen <james.chen@fourdesire.com>
2019-04-17 16:00:01 +0800

    add click events

---
+++ TicTacToe/Assets/scripts/GridSpace.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour {
+	
+	public Button m_button;
+	public Text m_text;
+	string m_player;
+
+	public void SetPlayer(string player) {
+		m_player = player;
+	}
+
+	public void SetSpace() {
+		m_text.text = m_player;
+		m_button.interactable = false;
+	}
+	void Start () {
+		m_text.text = "";
+	}
+	
+}
